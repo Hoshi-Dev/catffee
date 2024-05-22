d87f5e536dc3b6174125f09188aa0626dbb9b28f
Andrea Baez <andrea.baez037@gmail.com>
2024-05-22 18:59:51 -0300

    update header

--- js/script.js
+++ js/script.js
@@ -1,32 +1,57 @@
-//TO DO:
-// -> Revisar que el header no cambie el display de algunos elementos cuando se recarga.
-// -> Función para verificar el tamaño de pantalla.
 
-// Evento que toma el elemnto de menú en modo movil, y cambia de ícono
 const menuBtn = document.getElementById("js-menu-btn");
 const navbar = document.getElementById("js-header__nav");
 
-menuBtn.addEventListener("click", () => {
+// Función para alternar el menú de navegación en modo móvil
+function toggleMenu() {
   if (menuBtn.classList.contains("fa-bars")) {
     menuBtn.classList.remove("fa-bars");
-    menuBtn.classList.add("fa-xmark");
+    menuBtn.classList.add("fa-xmark"); 
+    navbar.classList.remove("hidden");
+    navbar.classList.add("block");
 
-    navbar.style.display = "block";
   } else {
     menuBtn.classList.remove("fa-xmark");
     menuBtn.classList.add("fa-bars");
-
-    navbar.style.display = "none";
+    navbar.classList.remove("block");
+    navbar.classList.add("hidden");
+    
   }
-});
+}
+// Agrega un event listener al botón del menú para alternar el menú al hacer clic
+menuBtn.addEventListener("click", toggleMenu);
 
-//Evento y función para verificar el tamaño de pantalla, ya que algunos elementos no respetan el display al recargar.
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 1024) {
-    navbar.style.display = "inline-flex";
-    menuBtn.style.display = "none";
-  } else{
-    navbar.style.display = "none";
-    menuBtn.style.display = "block";
+//Maneja la redimención de pantalla pantalla, para asegurarse de que los elementos se muestren o no
+function handleResize() {
+  if (window.innerWidth >= 1024) {
+    navbar.classList.add("block");
+    navbar.classList.remove("hidden");
+    menuBtn.classList.add("hidden");
+    menuBtn.classList.remove("block");
+    // Se asegura que el icono de menú se resetee para la vista de pantalla completa
+    if (menuBtn.classList.contains("fa-xmark")) {
+      menuBtn.classList.remove("fa-xmark");
+      menuBtn.classList.add("fa-bars");
+    }
+  } else {
+    navbar.classList.add("hidden");
+    navbar.classList.remove("block");
+    menuBtn.classList.add("block");
+    menuBtn.classList.remove("hidden");
   }
-});
+}
+
+// Función de debounce para mejorar el rendimiento al manejar eventos de redimensionamiento
+function debounce(func, wait = 50) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+// Agrega un event listener al evento de redimensionamiento de la ventana con debounce
+window.addEventListener('resize', debounce(handleResize));
+
+handleResize();
