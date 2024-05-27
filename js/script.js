1622dcae4c8a90b0d2c49083d1dd909c4a2219d1
Andrea Baez <andrea.baez037@gmail.com>
2024-05-27 19:52:16 -0300

    fix path to img

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ const cats = [
     edad: "5 años",
     peso: "8.5kg",
     descripcion: "Chapi es un adorable gato tuxedo, muy cariñoso y disfruta de la compañía humana. Le encanta acurrucarse y recibir mimos. Siempre está explorando y mostrando interés por su entorno. Su naturaleza curiosa lo mantiene entretenido y ocupado. Dado a su curiosidad es muy importante mantener tus alimentos bien vigilados, si no quieres que este pequeño travieso se coma tu merienda.",
-    imagen: "/assets/images/cat-1.webp"
+    imagen: "../assets/images/cat-1.webp"
   },
   {
     nombre: "mimi",
@@ -108,7 +108,7 @@ const cats = [
     edad: "3 años",
     peso: "4.2kg",
     descripcion: "Mimi es una gata siamesa elegante y juguetona. Le encanta perseguir juguetes y explorar su entorno. Es muy curiosa y siempre está buscando nuevas aventuras. Además, es muy cariñosa y disfruta de pasar tiempo con los clientes.",
-    imagen: "/assets/images/cat-2.webp"
+    imagen: "../assets/images/cat-2.webp"
   },
   {
     nombre: "tom",
@@ -116,7 +116,7 @@ const cats = [
     edad: "2 años",
     peso: "6.0kg",
     descripcion: "Tom es un gato persa tranquilo y relajado. Le gusta descansar en lugares cómodos y disfrutar de la tranquilidad del los rincones del café. Es muy sociable y se lleva bien con otros animales. Cuando se lo propone, va a hacer cualquier cosa por tal de que le des unos mimos y comida. Extorcionador 100%",
-    imagen: "/assets/images/cat-3.webp"
+    imagen: "../assets/images/cat-3.webp"
   }
 ];
 
